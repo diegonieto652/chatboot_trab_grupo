0b309153d1f9310ee61527681f22b4d74a06c9e8
diegonieto652 <37588382+diegonieto652@users.noreply.github.com>
2023-01-24 10:12:24 -0300

    Update chatboot.py

--- chatboot.py
+++ chatboot.py
@@ -1,117 +1,267 @@
-def questionarioinicio ():
-    print('''Digite uma opção abaixo
-1 PARA DÚVIDAS SOBRE EXAMES
-2 PARA DUVIDAS SOBRE CONSULTAS
-3 PARA FALAR COM UM ATENDENTE
-4 PARA SAIR''')
+##################################################################
+#  SENAC/RESILIA - Formação em Análise de Dados (FAD)            #
+#  Projeto em Grupo - Módulo 1 - Fale Comigo!                    #
+#  !/usr/bin/env python3                                         #
+#  -*- coding: utf-8 -*-                                         #
+#  Criado por: Diego de Arruda Nieto, Douglas Klem Portugal      #
+#  do Amaral, Matheus Barbosa Furtado e Yasmin Belo da Silva     #
+#  Data de criação: 12/01/2023                                   #
+#  versão = '3.11'                                               #
+##################################################################
+#
+# Atividade:
+#
+# Sua equipe foi chamada para criar um projeto que vai ajudar com atendimentos automatizados
+# de dúvidas sobre a empresa e no futuro vai coletar informações para auxiliar na tomada de decisão.
+# Desenvolvemos um código que lista opções de atendimento e o usuário é guiado por mensagens
+# avançando dentro desse atendimento simulado até que chegue ao final obtendo a resposta desejada.
+#
+# Escolhemos uma empresa no ramo de atuação com coleta labotatorial e clínica diagnóstica, como
+# mostrado a seguir:
+#
+print('#'*100)
+print('Bem-vindo ao chatbot da Clínica FAD! Aqui você encontra informações rápidas sobre nossos serviços.')
+print('#'*100)
 
-def questionario2 ():
-    print('''
-1 - BAIXAR EXAME
-2 - IMPRIMIR EXAME
-3 - ENVIAR EXAME PARA O MÉDICO
-4 - EXCLUIR EXAME''')
 
-def questionario3 ():
-    print('''
-1 - RETORNAR AO INICIO
-2 - SAIR''')
+def questionarioinicio():
+    print('''Digite uma das opções abaixo:
+1 EXAMES E RESULTADOS
+2 AGENDAMENTOS E CONSULTAS
+3 ORÇAMENTOS
+4 FALAR COM UM DE NOSSOS ATENDENTES
+5 SAIR''')
 
-def quesconsultas1 ():
+
+def questionario2():
     print('''
-    1 PARA VER CONSULTAS AGENDADAS
-    2 PARA CANCELAR CONSULTA
-    3 PARA REMARCAR CONSULTA
-    ''')
-def questionarioconsultas2 ():
+1 BAIXAR EXAME
+2 IMPRIMIR EXAME
+3 ENCAMINHAR EXAME PARA SEU MÉDICO
+4 EXCLUIR EXAME''')
+
+
+def questionario3():
+    print('''
+1 VOLTAR AO INÍCIO
+2 SAIR''')
+
+
+def quesconsultas1():
+    print('''
+1 VER CONSULTAS AGENDADAS
+2 CANCELAR UMA CONSULTA
+3 REMARCAR UMA CONSULTA''')
+
+
+def quesconsultas2():
+    print('''
+1 CONSULTAR PREÇOS DISPONÍVEIS
+2 ADICIONAR UM ORÇAMENTO SIMULADO
+3 REMOVER UM ORÇAMENTO SIMULADO''')
+
+
+def quesconsultas3():
+    print('''
+1 TELEFONES  
+2 REDES SOCIAIS
+3 ENCAMINHAMENTO INTERNO''')
+
+
+def questionarioconsultas2():
     print('''
     1 - ''')
-def separador ():
+
+
+def separador():
     print('*'*30)
 
 
 inicio = ''
 
-while inicio != '4':
+while inicio != '5':
     questionarioinicio()
     separador()
-    inicio = input('Qual é a opção desejada: ')
+    inicio = input('Qual é a opção desejada? ')
     separador()
     if inicio == '1':
         questionario2()
         separador()
-        questio2 = input('Qual é a opção desejada para exames: ')
+        questio2 = input('Qual é a opção desejada para exames? ')
         separador()
         if questio2 == '1':
-            print('clique aqui para baixar')
+            print('Clique AQUI para baixar seu exame.')
             questionario3()
-            quest3 = input('Qual é a opção desejada: ')
+            quest3 = input('Qual é a opção desejada? ')
             if quest3 == '1':
                 pass
             elif quest3 == '2':
                 print('Saindo...')
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
                 break
         if questio2 == '2':
-            print('clique aqui para imprimir')
+            print('Clique AQUI para imprimir seu exame.')
 
             questionario3()
-            quest3 = input('Qual é a opção desejada ')
+            quest3 = input('Qual é a opção desejada?')
             if quest3 == '1':
                 pass
             elif quest3 == '2':
-                print('Saindo...')
+                print('Saindo.')
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
                 break
             separador()
         if questio2 == '3':
-            print('clique aqui para enviar')
+            print('Clique AQUI para encaminhar seu exame.')
 
             questionario3()
-            quest3 = input('Qual é a opção desejada: ')
+            quest3 = input('Qual é a opção desejada?')
             if quest3 == '1':
                 pass
             elif quest3 == '2':
-                print('Saindo...')
+                print('Saindo.')
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
                 break
             separador()
         if questio2 == '4':
-            print('clique aqui para excluir')
+            print('Clique AQUI para excluir seu exame.')
 
             questionario3()
-            quest3 = input('Qual é a opção desejada: ')
+            quest3 = input('Qual é a opção desejada?')
             if quest3 == '1':
                 pass
             elif quest3 == '2':
-                print('Saindo...')
+                print('Saindo.')
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
                 break
             separador()
     elif inicio == '2':
         quesconsultas1()
-        questio1 = input("Digite uma opção")
-        if questio1 =='1':
-            print("Clique aqui para ver consultas agendadas")
+        separador()
+        questio1 = input('Qual é a opção desejada?')
+        separador()
+        if questio1 == '1':
+            print('Clique AQUI para ver suas consultas agendadas.')
             questionario3()
-            quest3= input("Digite uma opção")
-            if quest3 =='1':
-                pass
-            elif quest3 =='2':
-                print("Saindo...")
-                break
-
-        elif questio1 =='2':
-            print("Clique aqui para cancelar consultas")
-            questionario3()
-            quest3 = input('Digite uma opção')
-            if quest3 =='1':
+            quest3 = input('Qual é a opção desejada?')
+            if quest3 == '1':
                 pass
             elif quest3 == '2':
-                print("Saindo...")
+                print("Saindo.")
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
                 break
-        elif questio1 =='3':
-            print("Clique aqui para remarcar consultas")
+
+        elif questio1 == '2':
+            print('Clique AQUI para cancelar sua consulta.')
             questionario3()
-            quest3= input("Digite uma opção")
-            if quest3 =='1':
+            quest3 = input('Qual é a opção desejada?')
+            if quest3 == '1':
                 pass
-            elif quest3 =='2':
-                print("Saindo...")
+            elif quest3 == '2':
+                print('Saindo.')
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
                 break
+        elif questio1 == '3':
+            print('Clique AQUI para remarcar uma consulta previamente agendada.')
+            questionario3()
+            quest3 = input('Qual é a opção desejada?')
+            if quest3 == '1':
+                pass
+            elif quest3 == '2':
+                print('Saindo.')
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
+                break
+    elif inicio == '3':
+        quesconsultas2()
+        separador()
+        questio1 = input('Qual é a opção desejada?')
+        separador()
+        if questio1 == '1':
+            print(
+                'Clique AQUI para ver os preços de acordo com a tabela de exames atual.')
+            questionario3()
+            quest3 = input('Qual é a opção desejada?')
+            if quest3 == '1':
+                pass
+            elif quest3 == '2':
+                print("Saindo.")
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
+                break
+
+        elif questio1 == '2':
+            print('Clique AQUI para adicionar uma simulação de orçamento.')
+            questionario3()
+            quest3 = input('Qual é a opção desejada?')
+            if quest3 == '1':
+                pass
+            elif quest3 == '2':
+                print('Saindo.')
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
+                break
+        elif questio1 == '3':
+            print('Clique AQUI para remover uma simulação de orçamento.')
+            questionario3()
+            quest3 = input('Qual é a opção desejada?')
+            if quest3 == '1':
+                pass
+            elif quest3 == '2':
+                print('Saindo.')
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
+                break
+    elif inicio == '4':
+        quesconsultas3()
+        separador()
+        questio1 = input('Qual é a opção desejada?')
+        separador()
+        if questio1 == '1':
+            print(
+                'Clique AQUI para acessar os telefones disponíveis.')
+            questionario3()
+            quest3 = input('Qual é a opção desejada?')
+            if quest3 == '1':
+                pass
+            elif quest3 == '2':
+                print("Saindo.")
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
+                break
+
+        elif questio1 == '2':
+            print('Clique AQUI para acessar nossas redes sociais.')
+            questionario3()
+            quest3 = input('Qual é a opção desejada?')
+            if quest3 == '1':
+                pass
+            elif quest3 == '2':
+                print('Saindo.')
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
+                break
+        elif questio1 == '3':
+            print('Não encontrou meios de nos contactar? Deixe uma mensagem com suas informações que retornaremos o mais breve possível:')
+            questionario3()
+            quest3 = input('Qual é a opção desejada?')
+            if quest3 == '1':
+                pass
+            elif quest3 == '2':
+                print('Saindo.')
+                print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+                print('#'*100)
+                break
+else:
+    print('Obrigado por escolher a Clínica FAD! Volte sempre!')
+    print('#'*100)
+#
+# Referências (último acesso em 15/01/2023):
+# https://sergiofranco.com.br/
+# https://www.dremerson.com.br/
